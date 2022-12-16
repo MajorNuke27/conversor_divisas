8f35f6d62308840b71baf6afffb389379dfd6bec
Esau Montiel <montiel.orihuela.esau@gmail.com>
2022-12-16 02:10:00 +0000

    Implementacion del flujo principal del programa

--- src/conversor_divisas/Conversor_divisas.java
+++ src/conversor_divisas/Conversor_divisas.java
@@ -1,5 +1,18 @@
 package conversor_divisas;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import conversor_divisas.model.Divisa;
+import conversor_divisas.model.DivisaBase;
+import conversor_divisas.services.APIService;
+import conversor_divisas.services.DatabaseService;
+import conversor_divisas.services.DeserializationService;
+import conversor_divisas.view.Principal;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Esau Montiel
@@ -7,7 +20,33 @@ package conversor_divisas;
 public class Conversor_divisas {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try (DatabaseService bd = new DatabaseService()) { 
+            
+            ArrayList<Divisa> divisas = bd.getDivisas();
+            DivisaBase base = (DivisaBase) divisas.get(DatabaseService.INDEX_OF_BASE);
+            
+            if(!base.getFecha().equals(LocalDate.now())) {
+                String reponse = new APIService().getEquivalencias();
+                
+                divisas = DeserializationService.buildDivisasList(reponse);
+                base = (DivisaBase) divisas.get(DeserializationService.INDEX_OF_BASE);
+                
+                bd.setNewEquivalencias(base.getEquivalenciasMap(), base.getFecha());
+            }
+            
+            Principal view = new Principal(divisas, base.getFecha());
+            view.setVisible(true);
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error", "Ocurrio un problema con la base de datos, por favor reinicie el programa", JOptionPane.ERROR_MESSAGE);
+        } catch (JsonProcessingException ex) {
+            JOptionPane.showMessageDialog(null, "Error", "Ocurrio un problema al obtener las tazas de cambio.\n" + ex.toString(), JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex) {
+            System.out.println("error = " + ex.toString());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error", "Ocurrio un problema con la base de datos, por favor reinicie el programa", JOptionPane.ERROR_MESSAGE);
+        }
+        
     }
-    
 }
